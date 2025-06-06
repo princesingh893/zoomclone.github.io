e965a9c3be521e4966a8a4465f695721dc2718f1
Prince Singh <nageshwarpratapsingh1@gmail.com>
2025-06-06 05:00:20 +0000

    Complete reponsive design for phones

--- scripts.js
+++ scripts.js
@@ -23,3 +23,19 @@ let linkBackGround = document.querySelector("#link-background");
 linkBackGround.addEventListener("click", () => {
   linkBackGround.classList.add("bg");
 });
+
+const topDownHide = document.querySelector('.top-down-hide');
+const mainLeft = document.querySelector('.main-left');
+const main = document.querySelector('.main');
+
+topDownHide.addEventListener('click', () => {
+  console.log("topDownHide clicked");
+
+  if (mainLeft.style.display === "block") {
+    mainLeft.style.display = "none";
+  } else {
+    mainLeft.style.display = "block";
+  }
+  main.style.flexDirection = "column";
+  mainLeft.style.width = "100%";
+});
